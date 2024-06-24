196ca11fe71276f917dba606c1c040843b500163
awais-ur-rehman <awaisjarral37@gmail.com>
2024-06-24 15:02:38 +0500

    added login screen

---
+++ screens/Login/Login.js
@@ -0,0 +1,238 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import axios from "axios";
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const handleLogin = async () => {
+    let valid = true;
+
+    if (email === "") {
+      setEmailError("Email is required");
+      valid = false;
+    } else if (!validateEmail(email)) {
+      setEmailError("Invalid email format");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+
+    if (password === "") {
+      setPasswordError("Password is required");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    if (valid) {
+      try {
+        const response = await axios.post(
+          "https://starlabsapp-73ac6aee2550.herokuapp.com/api/users/login",
+          {
+            email,
+            password,
+          }
+        );
+
+        if (response.status === 200) {
+          navigation.navigate("PersonalInformation");
+        }
+      } catch (error) {
+        setModalMessage(error.response.data.msg || "Something went wrong!");
+        setModalVisible(true);
+      }
+    }
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleSignup = () => {
+    navigation.navigate("Signup");
+  };
+
+  const handleForgotPassword = () => {
+    // Handle forgot password functionality
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcomeText}>Welcome Back!</Text>
+      <Text style={styles.loginText}>Login to your account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#000"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#000"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      {passwordError ? (
+        <Text style={styles.errorText}>{passwordError}</Text>
+      ) : null}
+      <TouchableOpacity onPress={handleForgotPassword}>
+        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <View style={styles.signupContainer}>
+        <Text style={styles.signupText}>Didn't have an Account? </Text>
+        <TouchableOpacity onPress={handleSignup}>
+          <Text style={styles.signupLink}>Signup</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>OOPS!</Text>
+            <Text style={styles.modalText}>{modalMessage}</Text>
+            <TouchableOpacity style={styles.modalButton} onPress={closeModal}>
+              <Text style={styles.modalButtonText}>OK</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#000",
+  },
+  loginText: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: "#000",
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    marginBottom: 10,
+    color: "#000",
+  },
+  errorText: {
+    color: "red",
+    alignSelf: "flex-start",
+    marginBottom: 10,
+  },
+  forgotPasswordText: {
+    alignSelf: "flex-end",
+    color: "blue",
+    marginBottom: 20,
+  },
+  loginButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  signupContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  signupText: {
+    color: "#000",
+  },
+  signupLink: {
+    color: "blue",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+    textAlign: "center",
+  },
+  modalText: {
+    fontSize: 16,
+    color: "black",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  modalButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  modalButtonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+});
+
+export default LoginScreen;
