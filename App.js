ed6944f45d02d5b1eb7c5b41701cc29071a9898d
awais-ur-rehman <awaisjarral37@gmail.com>
2024-06-24 15:00:14 +0500

    added navigation

--- App.js
+++ App.js
@@ -3,15 +3,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import CalorieTrackerScreen from "./screens/CalorieTrackerScreen";
 import CameraScreen from "./screens/CameraScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import LoginScreen from "./screens/Login/Login";
+import SignupScreen from "./screens/Signup/Signup";
+import PersonalInformationScreen from "./screens/Goals/PersonalInformationScreen";
+import FitnessGoalsScreen from "./screens/Goals/FitnessGoalsScreen";
+import CaloriesScreen from "./screens/Goals/CaloriesScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Nutrition Track">
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Nutrition Track" component={CalorieTrackerScreen} />
         <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen
+          name="PersonalInformation"
+          component={PersonalInformationScreen}
+        />
+        <Stack.Screen name="FitnessGoals" component={FitnessGoalsScreen} />
+        <Stack.Screen name="Calories" component={CaloriesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
