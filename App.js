ff39cb21aa7707f44f1ce024df3fc5882e14f419
awais-ur-rehman <awaisjarral37@gmail.com>
2024-07-25 02:13:44 +0500

    added routes for screens

--- App.js
+++ App.js
@@ -9,28 +9,42 @@ import SignupScreen from "./screens/Signup/Signup";
 import PersonalInformationScreen from "./screens/Goals/PersonalInformationScreen";
 import FitnessGoalsScreen from "./screens/Goals/FitnessGoalsScreen";
 import CaloriesScreen from "./screens/Goals/CaloriesScreen";
+import ProductListScreen from "./screens/MarketPlace/ProductListScreen";
+import ProductDetailsScreen from "./screens/MarketPlace/ProductDetailsScreen";
+import CartScreen from "./screens/MarketPlace/CartScreen";
+import { CartProvider } from "./screens/MarketPlace/CartContext";
 
 const Stack = createStackNavigator();
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="Welcome"
-      >
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Nutrition Track" component={CalorieTrackerScreen} />
-        <Stack.Screen name="Camera" component={CameraScreen} />
-        <Stack.Screen
-          name="PersonalInformation"
-          component={PersonalInformationScreen}
-        />
-        <Stack.Screen name="FitnessGoals" component={FitnessGoalsScreen} />
-        <Stack.Screen name="Calories" component={CaloriesScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Welcome"
+        >
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen
+            name="Nutrition Track"
+            component={CalorieTrackerScreen}
+          />
+          <Stack.Screen name="Camera" component={CameraScreen} />
+          <Stack.Screen
+            name="PersonalInformation"
+            component={PersonalInformationScreen}
+          />
+          <Stack.Screen name="FitnessGoals" component={FitnessGoalsScreen} />
+          <Stack.Screen name="Calories" component={CaloriesScreen} />
+          <Stack.Screen name="ProductList" component={ProductListScreen} />
+          <Stack.Screen
+            name="ProductDetails"
+            component={ProductDetailsScreen}
+          />
+          <Stack.Screen name="Cart" component={CartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
